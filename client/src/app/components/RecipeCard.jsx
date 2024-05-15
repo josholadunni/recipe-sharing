d2f4536177a7f8e9640df494ca2d8be98a3dc8cf
Josh Oladunni <josholadunni@gmail.com>
2024-05-15 16:36:01 +0100

    Added Image component and fixed layout

--- client/src/app/components/RecipeCard.jsx
+++ client/src/app/components/RecipeCard.jsx
@@ -5,44 +5,30 @@ import { useState } from "react";
 import "../../styles/RecipeCard.css";
 
 const RecipeCard = (props) => {
-  const [orientation, setOrientation] = useState("portrait");
-
-  const handleImageLoad = (event) => {
-    console.log("Image loaded");
-    const { naturalWidth, naturalHeight } = event.target;
-    const newOrientation =
-      naturalWidth - 100 > naturalHeight ? "landscape" : "portrait";
-    setOrientation(newOrientation);
-  };
-
   return (
     <div className="recipe-card-container">
-      <div className={`recipe-card`}>
-        <div className={`recipe-card-image-container-${orientation}`}>
-          <Image
-            width={300}
-            height={300}
-            className={`recipe-card-image-${orientation}`}
-            src={"/images/" + props.imgFileName}
-            alt={props.title + " recipe"}
-            onLoadingComplete={handleImageLoad}
-          />
-        </div>
-        <div className="recipe-card-bottom">
-          <div className="recipe-card-title-likes">
-            <h2 className="recipe-card-title">{props.title}</h2>
-            <div className="recipe-card-likes">
-              <button>Like</button>
-              100 Likes
-            </div>
+      <div className="recipe-card-img">
+        <Image
+          src={"/images/" + props.imgFileName}
+          alt={props.title + " recipe"}
+          fill={true}
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+      <div className="recipe-card-bottom">
+        <div className="recipe-card-title-likes">
+          <h2 className="recipe-card-title">{props.title}</h2>
+          <div className="recipe-card-likes">
+            <button>Like</button>
+            100 Likes
           </div>
-          <p className="recipe-card-description">{props.description}</p>
-          <p>
-            {props.categories.map((category) => {
-              return `${category} `;
-            })}
-          </p>
         </div>
+        <p className="recipe-card-description">{props.description}</p>
+        <p>
+          {props.categories.map((category) => {
+            return `${category} `;
+          })}
+        </p>
       </div>
     </div>
   );
