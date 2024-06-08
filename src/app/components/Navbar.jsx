bccac17ea224bddb903d3f78a9baf90aa9494ce4
Josh Oladunni <josholadunni@gmail.com>
2024-06-08 20:39:13 +0100

    About to explore removing the express backend so that it's just next.js

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
 function Navbar() {
   return (
     <nav className="pt-5">
       <ul className="text-center">
-        <li>Home</li>
+        <Link href="/">Home</Link>
         <li>Browse</li>
         <li>Search</li>
+        <Link href="/add-recipe">Add Recipe</Link>
         <li>Login</li>
       </ul>
     </nav>
--- src/app/page.js
+++ src/app/page.js
@@ -3,37 +3,6 @@ import "../styles/Home.css";
 import PopularRecipes from "./components/Home/PopularRecipes.jsx";
 import RecentRecipes from "./components/Home/RecentRecipes.jsx";
 
-// const popularRecipes = [
-//   {
-//     id: 1,
-//     title: "Tasty Tacos",
-//     imgFileName: "IMG_0794.jpeg",
-//     description: "Spicy tacos from Mexico",
-//     categories: ["Mexican", "Meat", "Spicy"],
-//   },
-//   {
-//     id: 2,
-//     title: "Homemade Pizza",
-//     imgFileName: "IMG_0795.jpeg",
-//     description: "Cheap and customisable",
-//     categories: ["Italian"],
-//   },
-//   {
-//     id: 3,
-//     title: "Simple Salad",
-//     imgFileName: "IMG_0796.jpeg",
-//     description: "Rustle this up in 5 minutes and enjoy",
-//     categories: ["Italian"],
-//   },
-//   {
-//     id: 4,
-//     title: "Chocolate Brownies",
-//     imgFileName: "IMG_0797.webp",
-//     description: "Best warm and served with vanilla ice cream",
-//     categories: ["Dessert", "Chocolate"],
-//   },
-// ];
-
 export default async function Home() {
   return (
     <div>
