a2262d88f6401c6a1ae5a705dd8f43989f8f26a3
Josh Oladunni <josholadunni@gmail.com>
2024-06-23 21:29:16 +0100

    Added basic registration form. Next - hashing passwords with bcrypt

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,5 +2,10 @@ import React from "react";
 import LoginForm from "../components/LoginForm";
 
 export default function LogIn() {
-  return <LoginForm />;
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-medium pb-3 mt-5">Log In</h1>
+      <LoginForm />
+    </div>
+  );
 }
---
+++ src/app/register/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import RegisterForm from "../components/RegisterForm";
+
+export default function LogIn() {
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-medium pb-3 mt-5">Sign Up</h1>
+      <RegisterForm />
+    </div>
+  );
+}
