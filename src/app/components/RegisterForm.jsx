10a6b923c1329add2d6695ff6baee20cf98b748a
Josh Oladunni <josholadunni@gmail.com>
2024-06-27 22:22:20 +0100

    Added feedback message on success & failure. Next - continuing settint up authentication and authorization

--- src/app/components/RegisterForm.jsx
+++ src/app/components/RegisterForm.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Input from "./Input.jsx";
 import { createUser } from "../lib/actions.js";
 
 export default function RegisterForm() {
+  const [message, setMessage] = useState("");
+  const handleSubmit = async (formData) => {
+    const result = await createUser(formData);
+
+    if (result.success) {
+      setMessage(result.message);
+    } else {
+      setMessage(result.message);
+    }
+  };
   return (
     <div>
-      <form action={createUser}>
+      <form action={handleSubmit}>
         <div>
           <Input
             label="Username"
@@ -36,6 +48,17 @@ export default function RegisterForm() {
           </button>
         </div>
       </form>
+      {message && (
+        <div
+          className={`mt-4 p-2 ${
+            message.includes("successfully")
+              ? "bg-green-100 text-green-700"
+              : "bg-red-100 text-red-700"
+          }`}
+        >
+          {message}
+        </div>
+      )}
     </div>
   );
 }
