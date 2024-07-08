0cd72072227da251dc0e35a9a400b6fd9c0a236c
Josh Oladunni <josholadunni@gmail.com>
2024-07-08 18:22:01 +0200

    Log in, out and route protection now working

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,15 +1,31 @@
 import Link from "next/link";
+import { signOut } from "@/auth";
 function Navbar() {
   return (
-    <nav className="pt-5">
-      <ul className="text-center">
-        <Link href="/">Home</Link>
-        <li>Browse</li>
-        <li>Search</li>
-        <Link href="/add-recipe">Add Recipe</Link>
-        <li>Login</li>
-      </ul>
-    </nav>
+    <div>
+      <nav className="pt-5">
+        <ul className="text-center">
+          <Link href="/">Home</Link>
+          <li>Browse</li>
+          <li>Search</li>
+          <Link href="/add-recipe">Add Recipe</Link>
+          <li>Login</li>
+          <li>
+            {" "}
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+                <div className="hidden md:block">Sign Out</div>
+              </button>
+            </form>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
