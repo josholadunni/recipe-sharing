a46aed988657ebe8c998a683d7ceb4ad292c5c8b
Josh Oladunni <josholadunni@gmail.com>
2024-07-13 23:19:53 +0100

    About to look into reconfiguring next-auth setup. May need to create an api directory in order to get sessionProvider working.

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,18 +4,16 @@ import Link from "next/link";
 import { logOut } from "@/app/lib/actions";
 import { useRouter } from "next/navigation.js";
 import { useFormState } from "react-dom";
+import { useSession } from "next-auth/react";
 import React from "react";
 
 function Navbar() {
   const router = useRouter();
   const [state, formAction] = useFormState(logOut, { isLoggedOut: false });
-  console.log("Initial state:", { ...state });
+  const { data: session, status } = useSession();
 
   React.useEffect(() => {
-    console.log("Render - state: " + state);
     if (state?.isLoggedOut) {
-      console.log("Client: Success, preparing to redirect");
-      console.log("Client: Redirecting to home");
       router.push("/");
     }
   }, [state, router]);
@@ -29,14 +27,15 @@ function Navbar() {
           <li>Search</li>
           <Link href="/add-recipe">Add Recipe</Link>
           <li>Login</li>
-          <li>
-            {" "}
-            <form action={formAction}>
-              <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-                <div className="hidden md:block">Sign Out</div>
-              </button>
-            </form>
-          </li>
+          {status === "authenticated" && (
+            <li>
+              <form action={formAction}>
+                <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+                  <div className="hidden md:block">Sign Out</div>
+                </button>
+              </form>
+            </li>
+          )}
         </ul>
       </nav>
     </div>
