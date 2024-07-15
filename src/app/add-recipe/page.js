6da225906db3da28032ddcb68f728fa9f10703a0
Josh Oladunni <josholadunni@gmail.com>
2024-07-15 18:34:14 +0100

    Added user to recipe uploads based on session data (auth())

--- src/app/add-recipe/page.js
+++ src/app/add-recipe/page.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { fetchCategories } from "../lib/models/RecipeCategory.js";
 import { createCategories } from "../lib/models/RecipeCategory.js";
-import Form from "../components/Form.jsx";
+import RecipeForm from "../components/RecipeForm.jsx";
 import H1 from "../components/H1.jsx";
+import { auth } from "../auth.js";
 
 export default async function AddRecipes() {
   createCategories();
   const categories = await fetchCategories();
   const categoryNames = categories.map((category) => category.name);
-
   return (
     <div>
       <H1 text="Add Recipe" />
-      <Form categoryNames={categoryNames} />
+      <RecipeForm categoryNames={categoryNames} />
     </div>
   );
 }
