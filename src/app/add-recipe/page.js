9a222d8f0abd38bed5ec0ee6d4f0137239c4bd31
Josh Oladunni <josholadunni@gmail.com>
2024-07-21 22:24:49 +0100

    Revert "Added user to recipe uploads based on session data (auth())"
    This reverts commit 5cb83e7eb7ed9f2202a8c5a31c72ca44267823d2.

--- src/app/add-recipe/page.js
+++ src/app/add-recipe/page.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { fetchCategories } from "../lib/models/RecipeCategory.js";
 import { createCategories } from "../lib/models/RecipeCategory.js";
-import RecipeForm from "../components/RecipeForm.jsx";
+import Form from "../components/Form.jsx";
 import H1 from "../components/H1.jsx";
-import { auth } from "../auth.js";
 
 export default async function AddRecipes() {
   createCategories();
   const categories = await fetchCategories();
   const categoryNames = categories.map((category) => category.name);
+
   return (
     <div>
       <H1 text="Add Recipe" />
-      <RecipeForm categoryNames={categoryNames} />
+      <Form categoryNames={categoryNames} />
     </div>
   );
 }
