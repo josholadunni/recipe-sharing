62245825474b8645fc5fe15a1637a6380007086f
Josh Oladunni <josholadunni@gmail.com>
2024-08-04 20:00:21 +0100

    Changed upload file button on add recipe form to upload recipe. Roughly styled this button and the add ingredient/method steps buttons

--- src/app/components/SubmitButton.jsx
+++ src/app/components/SubmitButton.jsx
@@ -6,8 +6,12 @@ export function SubmitButton() {
   const { pending } = useFormStatus();
 
   return (
-    <button type="submit" aria-disabled={pending}>
-      {pending ? "Uploading..." : "Upload File"}
+    <button
+      className=" bg-white text-black border border-black rounded hover:bg-black hover:text-white p-1 mt-12"
+      type="submit"
+      aria-disabled={pending}
+    >
+      {pending ? "Uploading..." : "Upload Recipe"}
     </button>
   );
 }
