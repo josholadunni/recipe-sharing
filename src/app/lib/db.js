0bd2264f08b74ef33ed96d8b06fb30dce50288b7
josho <josholadunni@gmail.com>
2024-08-18 00:30:14 +0100

    Linked DB to digital ocean one. Also fixed error on recipe creation caused by findBy function now returning an object@

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,11 +1,11 @@
 import pg from "pg";
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("recipe_sharing", "PLACEHOLDER_DB_DIALECT", "hpharrypot", {
-  host: "localhost",
+const sequelize = new Sequelize("PLACEHOLDER_DB_NAME", "PLACEHOLDER_DB_DIALECT", "hpharrypot", {
+  host: "PLACEHOLDER_DB_HOST",
   dialect: "PLACEHOLDER_DB_DIALECT",
   dialectModule: pg,
-  port: 5433,
+  port: 5432,
 });
 
 export default sequelize;
