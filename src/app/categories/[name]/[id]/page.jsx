7869e081ef653ceba0ae595d98a132ae3f7af592
josho <josholadunni@gmail.com>
2024-08-18 15:42:30 +0100

    Added recipe category page that displays all recipes under a category

--- src/app/categories/[name]/[id]/page.jsx
+++ src/app/categories/[name]/[id]/page.jsx
@@ -1,8 +1,45 @@
 import { fetchRecipesByCategoryId } from "../../../lib/data";
+import RecipeCard from "../../../components/RecipeCard";
+import H1 from "../../../components/H1";
+import { fetchRecipeLikes } from "../../../lib/data";
+
 export default async function CategoryPage(params) {
+  let likeRecipeId = undefined;
+  let renderedRecipes = undefined;
   const { name, id } = params.params;
 
-  console.log(await fetchRecipesByCategoryId(id));
+  const recipeList = await fetchRecipesByCategoryId(id);
 
-  return <h1>Category: {name}</h1>;
+  const allLikes = await fetchRecipeLikes();
+  if (allLikes) {
+    likeRecipeId = allLikes.map((like) => like.dataValues.RecipeId);
+  }
+
+  if (recipeList) {
+    renderedRecipes = recipeList.map((recipe) => {
+      const categories = recipe.RecipeCategories.map(
+        (category) => category.name
+      );
+      // console.log(recipe.dataValues.name);
+      return (
+        <RecipeCard
+          key={recipe.dataValues.id}
+          id={recipe.dataValues.id}
+          title={recipe.dataValues.name}
+          imgFileName={recipe.dataValues.imageURL}
+          description={recipe.dataValues.short_description}
+          likes={likeRecipeId.filter((like) => like === recipe.id).length}
+          categories={categories}
+          username={recipe.username}
+        />
+      );
+    });
+  }
+
+  return (
+    <>
+      <H1 text={`${name} Recipes`}></H1>
+      {renderedRecipes}
+    </>
+  );
 }
