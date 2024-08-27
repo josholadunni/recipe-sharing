6659dfe57dc74417c58ec89d056c7a2ba5bd3245
josho <josholadunni@gmail.com>
2024-08-27 21:27:57 +0100

    Started adding static params to categories, added recipe name to recipe URL. Need to finish off category static params@

---
+++ src/app/recipes/[name]/[id]/page.jsx
@@ -0,0 +1,60 @@
+import { fetchAllRecipes, fetchRecipeById } from "../../../lib/data";
+import H1 from "../../../components/H1";
+import Image from "next/image";
+
+export async function generateStaticParams() {
+  const recipes = await fetchAllRecipes();
+  return recipes.map((recipe) => ({
+    name: recipe.dataValues.name.toString().replace(/\s+/g, "-").toLowerCase(),
+    id: recipe.dataValues.id.toString(),
+  }));
+}
+
+export default async function RecipePage(params) {
+  console.log(params);
+  const { name, id } = params.params;
+  const recipe = await fetchRecipeById(id);
+
+  if (!recipe) {
+    return <div>Recipe not found</div>;
+  }
+
+  const renderIngredients = () => {
+    const ingredients = recipe.ingredients;
+    return (
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient}</li>
+        ))}
+      </ul>
+    );
+  };
+
+  const renderMethod = () => {
+    const method = recipe.method;
+    return (
+      <ul>
+        {method.map((methodStep) => (
+          <li>{methodStep}</li>
+        ))}
+      </ul>
+    );
+  };
+
+  return (
+    <div>
+      <H1 text={recipe.name}></H1>
+      <div className="relative h-72 w-60">
+        <Image
+          src={recipe.imageURL}
+          alt={recipe.title + " recipe"}
+          fill={true}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          className="object-cover"
+        />
+      </div>
+      <div>{renderIngredients()}</div>
+      <div>{renderMethod()}</div>
+    </div>
+  );
+}
