098edc60d3f1fab2a1169b98fc36d80f113df042
Josh Oladunni <josholadunni@gmail.com>
2024-09-30 18:30:45 +0100

    Changed format of upload time to days/weeks. Next - implementing now/minutes/hours

--- src/app/lib/utils.js
+++ src/app/lib/utils.js
@@ -1,8 +1,32 @@
+import { number } from "zod";
+
 export function capitalizeFirstLetter(word) {
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
 export function formatDate(dateString) {
-  const options = { year: "numeric", month: "long", day: "numeric" };
-  return new Date(dateString).toLocaleDateString(undefined, options);
+  const uploadDate = new Date(dateString);
+  const uploadDay = uploadDate.getDate();
+
+  const currentDate = new Date();
+  const currentDay = currentDate.getDate();
+
+  const formattedDate = () => {
+    const numberOfDays = currentDay - uploadDay;
+
+    if (numberOfDays >= 7) {
+      const numberOfWeeks = numberOfDays / 7;
+      if (numberOfWeeks < 2) {
+        return `${String(numberOfWeeks).slice(0, 1)} week ago`;
+      } else {
+        return `${String(numberOfWeeks).slice(0, 1)} weeks ago`;
+      }
+    } else if (numberOfDays == 1) {
+      return `${numberOfDays} day go`;
+    } else {
+      return `${numberOfDays} days go`;
+    }
+  };
+
+  return `${formattedDate()}`;
 }
