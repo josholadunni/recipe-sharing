08c289658ef11fbc66bdf2035cf797b926860e64
Josh Oladunni <josholadunni@gmail.com>
2024-11-10 23:22:42 +0000

    Stage 2 of styling the recipe apage - almost finished layouts for mobile and sm screens

--- src/app/recipes/[name]/[id]/page.jsx
+++ src/app/recipes/[name]/[id]/page.jsx
@@ -1,5 +1,6 @@
 import { fetchAllRecipes, fetchRecipeById } from "../../../lib/data";
 import H1 from "../../../components/H1";
+import H2 from "../../../components/H2";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -22,24 +23,38 @@ export default async function RecipePage(params) {
   const renderIngredients = () => {
     const ingredients = recipe.ingredients;
     return (
-      <ul>
-        {ingredients.map((ingredient, index) => (
-          <li key={index}>{ingredient}</li>
-        ))}
-      </ul>
+      <>
+        <div>
+          <h2 style={{ fontWeight: 600 }} className="text-left text-xl mb-3">
+            Ingredients
+          </h2>
+        </div>
+        <ul>
+          {ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+          ))}
+        </ul>
+      </>
     );
   };
 
   const renderMethod = () => {
     const method = recipe.method;
     return (
-      <ul>
-        {method.map((methodStep, index) => (
-          <li key={index}>{methodStep}</li>
-        ))}
-      </ul>
+      <>
+        <h2 style={{ fontWeight: 600 }} className="text-left text-xl my-4">
+          Method
+        </h2>
+        <ul>
+          {method.map((methodStep, index) => (
+            <li key={index}>{methodStep}</li>
+          ))}
+        </ul>
+      </>
     );
   };
+
+  const username = recipe.username.toLowerCase();
 
   const renderCategories = () => {
     return recipe.RecipeCategories.map((category, index) => {
@@ -82,20 +97,79 @@ export default async function RecipePage(params) {
   };
 
   return (
-    <div className="flex flex-col relative top-12 mx-20 sm:mx-36 md:mx-30">
-      <H1 text={recipe.name}></H1>
-      <div className="flex justify-center mt-10 w-auto h-96 relative">
-        <Image
-          src={recipe.imageURL}
-          alt={recipe.title + " recipe"}
-          width={500}
-          height={500}
-          className="object-cover rounded-t-lg"
-        />
+    <div className="flex flex-wrap w-full justify-center relative top-12 mx-auto px-10">
+      {/* Mobile */}
+      <div className="min-w-[20rem] md:w-1/3 sm:hidden">
+        <H1 text={recipe.name}></H1>
+        <div className="flex mt-2 justify-center">
+          <Link
+            className="text-center text-orange-600 underline text-under"
+            href={`/users/${username}`}
+          >
+            {username}
+          </Link>
+        </div>
+        <div className="flex flex-col justify-center mt-5 w-full relative">
+          <div class="mx-auto">
+            <Image
+              src={recipe.imageURL}
+              alt={recipe.title + " recipe"}
+              width={350}
+              height={350}
+              className="object-cover rounded-t-lg"
+            />
+            <div className="mt-7">{renderCategories()}</div>
+            <div className="flex-1 mt-7 min-w-[20rem] md:w-1/3">
+              {renderIngredients()}
+            </div>
+            <div className="flex-1 min-w-[20rem] md:w-1/3 mt-6">
+              {renderMethod()}
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="font-bold mt-6">{renderCategories()}</div>
-      <div>{renderIngredients()}</div>
-      <div>{renderMethod()}</div>
+      {/* Larger */}
+      <div className="min-w-[25rem] hidden sm:block sm:w-full">
+        <div className="flex flex-row justify-center mt-12 w-full relative">
+          <div className="basis-1/2 justify-center">
+            <Image
+              src={recipe.imageURL}
+              alt={recipe.title + " recipe"}
+              width={350}
+              height={350}
+              className="object-cover rounded-t-lg"
+            />
+          </div>
+          <div className="flex flex-col align-middle my-auto basis-1/2 ml-7">
+            {
+              <>
+                <h1
+                  style={{ fontWeight: 900 }}
+                  className="text-3xl font-medium pb-3"
+                >
+                  {recipe.name}
+                </h1>
+                <div className="flex">
+                  <Link
+                    className="text-center text-orange-600 underline text-under"
+                    href={`/users/${username}`}
+                  >
+                    {username}
+                  </Link>
+                </div>
+              </>
+            }
+            <div className="mt-10">{recipe.short_description}</div>
+            <div className="mt-7">{renderCategories()}</div>
+          </div>
+        </div>
+
+        {/* Ingredients and method section */}
+        <div className="flex flex-col pl-5">
+          <div className="flex flex-col basis-1/2 mt-10">{renderMethod()}</div>
+          <div className="basis 1/2 mt-10">{renderIngredients()}</div>
+        </div>
+      </div>
     </div>
   );
 }
