0111ba70e8a3d105772c59248973546c201a14c0
Josh Oladunni <josholadunni@gmail.com>
2025-02-25 20:46:04 +0000

    Ready to make public

--- config/config.js
+++ config/config.js
@@ -2,24 +2,24 @@
 
 const config = {
   development: {
-    username: "PLACEHOLDER_DB_DIALECT",
-    password: "hpharrypot",
-    database: "recipe-sharing",
-    host: "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "PLACEHOLDER_DB_DIALECT",
   },
   test: {
-    username: "PLACEHOLDER_DB_DIALECT",
-    password: "hpharrypot",
-    database: "recipe-sharing",
-    host: "127.0.0.1",
+    username: process.env.DB_USER_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST_TEST,
     dialect: "PLACEHOLDER_DB_DIALECT",
   },
   production: {
-    username: "PLACEHOLDER_DB_DIALECT",
-    password: "hpharrypot",
-    database: "recipe-sharing",
-    host: "127.0.0.1",
+    username: process.env.DB_USER_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_NAME_PROD,
+    host: process.env.DB_HOST_PROD,
     dialect: "PLACEHOLDER_DB_DIALECT",
   },
 };
--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,11 +1,16 @@
 import pg from "pg";
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("PLACEHOLDER_DB_NAME", "PLACEHOLDER_DB_DIALECT", "hpharrypot", {
-  host: "PLACEHOLDER_DB_HOST",
-  dialect: "PLACEHOLDER_DB_DIALECT",
-  dialectModule: pg,
-  port: 5432,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "PLACEHOLDER_DB_DIALECT",
+    dialectModule: pg,
+    port: process.env.DB_PORT || 5432,
+  }
+);
 
 export default sequelize;
