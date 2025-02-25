d4df82bb858f4052d8b9aecfc1f7f0dd046391a9
Josh Oladunni <josholadunni@gmail.com>
2025-02-25 20:46:04 +0000

    Ready to make public

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,11 +1,16 @@
 import pg from "pg";
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("recipesharingdev", "postgres", "hpharrypot", {
-  host: "64.227.44.193",
-  dialect: "postgres",
-  dialectModule: pg,
-  port: 5432,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    dialectModule: pg,
+    port: process.env.DB_PORT || 5432,
+  }
+);
 
 export default sequelize;
