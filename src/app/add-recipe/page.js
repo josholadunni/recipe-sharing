34603dbab84a237af2ce66744e17cf6c7cfef803
Josh Oladunni <josholadunni@gmail.com>
2025-03-25 21:26:56 +0000

    Added extra margin above add recipe form

--- src/app/add-recipe/page.js
+++ src/app/add-recipe/page.js
@@ -5,7 +5,7 @@ import H1 from "../components/H1.jsx";
 export default async function AddRecipes() {
   return (
     <div className="flex justify-center">
-      <div className="bg-white w-fit min-h-[600px] mt-10 rounded-t-2xl shadow-md px-32 py-4 relative">
+      <div className="bg-white w-fit min-h-[600px] mt-24 rounded-t-2xl shadow-md px-32 py-4 relative">
         <H1 text="Add Recipe" className="text-center" />
         <Form />
       </div>
