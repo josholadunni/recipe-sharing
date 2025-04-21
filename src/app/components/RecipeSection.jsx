274c64599185fd00af4d39931dd4ef582ed63d28
Josh Oladunni <josholadunni@gmail.com>
2025-04-21 22:39:36 +0100

    Added useEffect and state updates with the data fetching in RecipeSection to allow for state-based loading skeletons. Made data.js use server

--- src/app/components/RecipeSection.jsx
+++ src/app/components/RecipeSection.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import ContentWithSkeleton from "./Wrappers/ContentWithSkeleton";
 import RecipeGrid from "./Home/RecipeGrid";
 import RecipeCarousel from "./Home/RecipeCarousel";
 
-async function RecipeSection({
+function RecipeSection({
   currentUserId,
   fetchRecipeLikes,
   fetchRecipes,
@@ -11,32 +12,54 @@ async function RecipeSection({
   isDeletable,
   layout,
 }) {
-  const [allLikes, recipes] = await Promise.all([
-    fetchRecipeLikes(),
-    fetchRecipes(categoryId),
-  ]);
-  const serializedData = {
-    recipes: JSON.parse(JSON.stringify(recipes)),
-    allLikes: JSON.parse(JSON.stringify(allLikes)),
-    // currentUserId: currentUserId?.result || null,
-  };
+  const [recipes, setRecipes] = useState(null);
+  const [likes, setLikes] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    //Load recipes and update state once loaded
+    async function loadRecipes() {
+      try {
+        const [allLikes, recipes] = await Promise.all([
+          fetchRecipeLikes(),
+          fetchRecipes(categoryId),
+        ]);
+        //Serialize data from data fetches
+        const serializedData = {
+          recipes: JSON.parse(JSON.stringify(recipes)),
+          allLikes: JSON.parse(JSON.stringify(allLikes)),
+          // currentUserId: currentUserId?.result || null,
+        };
+        //Update states with serialized data
+        setRecipes(serializedData.recipes);
+        setLikes(serializedData.allLikes);
+
+        // Add a small delay to make the transition smoother
+        setTimeout(() => {
+          setIsLoaded(true);
+        }, 300);
+      } catch (error) {
+        console.error("Error loading recipes:", error);
+      }
+    }
+
+    loadRecipes();
+  }, [fetchRecipeLikes, fetchRecipes, categoryId]);
   return (
     <div>
-      <ContentWithSkeleton
-        data={(serializedData.allLikes, serializedData.recipes)}
-      >
+      <ContentWithSkeleton data={(likes, recipes)}>
         {layout === "grid" ? (
           <RecipeGrid
-            allLikes={serializedData.allLikes}
-            recipes={serializedData.recipes}
+            allLikes={likes}
+            recipes={recipes}
             currentUserId={currentUserId}
             deleteButton={isDeletable}
           />
         ) : (
           <div className="flex">
             <RecipeCarousel
-              allLikes={serializedData.allLikes}
-              recipes={serializedData.recipes}
+              allLikes={likes}
+              recipes={recipes}
               currentUserId={currentUserId}
               deleteButton={isDeletable}
             />
